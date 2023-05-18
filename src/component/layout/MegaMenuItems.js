e273cdfd67c0e407331b20760060b5c639b13196
omidhldn <omidhldn@gmail.com>
2023-05-18 23:30:29 +0330

    28-02-1402

--- src/component/layout/MegaMenuItems.js
+++ src/component/layout/MegaMenuItems.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import MegaMenuItem from './MegaMenuItem';
 import classes from './MegaMenuItems.module.css';
 import MegaMenuItemsGroup from './MegaMenuItemsGroup';
@@ -5,14 +6,17 @@ import { GROUPS } from './assets/data/data';
 
 
 const MegaMenuItems = (props) => {
+  // const [selectedGroup, setSelectedGroup] = useState({"sub-group":[{title:'',items:[]}]});
+  const [bodyContent, setBodyContent] = useState(null);
+
   const getIdHandler = (id) => {
     // console.log(id);
     const findGroup = GROUPS.find(el => {
       return el.id === id;
     });
     // console.log(findGroup);
-
-
+    const JSXcontent = findGroup['sub-group'].map(el => <MegaMenuItem subgroup={el}/>);
+    setBodyContent(JSXcontent);
   };
 
   return(
@@ -25,7 +29,9 @@ const MegaMenuItems = (props) => {
         </div>
         <div className={classes['mega-menu-body']}>
           <div className={classes['flex-container']}>
-          <MegaMenuItem subgroup={GROUPS[0]['sub-group'][0]}/>
+          {/* <MegaMenuItem subgroup={GROUPS[0]['sub-group'][0]}/> */}
+          {bodyContent}
+          {/* {console.log(selectedGroup['sub-group'])} */}
           </div>
         </div>
       </div>
